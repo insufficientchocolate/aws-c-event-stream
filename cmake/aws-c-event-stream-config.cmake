acdf7fd2b8e97982026b265fc6d3d477e1c2ad1a
Jerry Duan <Jerry.Duan@ortery.com>
2019-01-08 15:21:26 +0800

    hunterization: use CMakePackageConfigHelpers. removed components.

--- cmake/aws-c-event-stream-config.cmake
+++ cmake/aws-c-event-stream-config.cmake
@@ -1,6 +1,8 @@
+@PACKAGE_INIT@
 include(CMakeFindDependencyMacro)
-find_dependency(aws-c-common)
-find_dependency(aws-checksums)
+find_package(aws-c-common CONFIG REQUIRED)
+find_package(aws-checksums CONFIG REQUIRED)
 
 include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
+check_required_components(@CMAKE_PROJECT_NAME@)
 
